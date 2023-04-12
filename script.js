ff0072bd6c5e4b157e9aa5bdbb696c93cd6d3eff
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-04-12 15:02:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species= species;
+	}
+	get getspecies(species){
+		return this.species;
+	}
+	makeSound(){
+		console.log("animal sound");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
